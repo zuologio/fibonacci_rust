974c12dccec1683e45e39450dcc221a6800d2418
g10 <zuologio@hotmail.com>
2019-08-18 15:33:21 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
+
+#[wasm_bindgen]
+pub fn fibonacci(index: i32) -> i64 {
+    if index <= 1 {
+        index.into()
+    } else {
+        fibonacci(index - 1) + fibonacci(index - 2)
+    }
+}
